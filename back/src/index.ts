ca76ce21db99db5bffa251354198f25dcdc186b1
mannyjv <emmanuel.vazquez06@gmail.com>
2023-05-26 14:55:38 +0200

    add registration form validation

--- back/src/index.ts
+++ back/src/index.ts
@@ -43,7 +43,6 @@ const start = async (): Promise<void> => {
   });
   const server = new ApolloServer({
     schema: applyMiddleware(schema, permissions),
-    // schema,
     context: ({ req }) => {
       if (
         req.headers.authorization === undefined ||
@@ -71,7 +70,6 @@ const start = async (): Promise<void> => {
       origin: ["http://localhost:3000", "https://studio.apollographql.com"],
       credentials: true,
     },
-    // plugins: [permissions],
   });
 
   try {
